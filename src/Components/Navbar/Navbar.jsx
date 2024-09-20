1941dcfee089c8b724dc21516f790a2476fd9133
Rajan <rajan.rana6@gmail.com>
2024-09-20 10:03:35 +0530

    create nav bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div>
+            <Navbar bg="light" expand="lg" className="px-3">
+                <Navbar.Brand as={Link} to="/">
+                    E-Shop
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/products">Products</Nav.Link>
+                        <Nav.Link as={Link} to="/about">About Us</Nav.Link>
+                        <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                    </Nav>
+                    <Form className="d-flex">
+                        <FormControl
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                    <Nav>
+                        <Nav.Link as={Link} to="/cart">
+                            <i className="fas fa-shopping-cart"></i> Cart
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/login">
+                            <i className="fas fa-user"></i> Login
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
     )
 }
 
