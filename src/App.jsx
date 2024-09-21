cedef92e7caead49c45763792792b86fb314c9ff
Rajan <rajan.rana6@gmail.com>
2024-09-21 21:07:30 +0530

    navbar create

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import Product from './Components/Product/Product'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Product />
     </>
   )
 }
