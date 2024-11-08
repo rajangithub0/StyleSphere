524b1a8dc527ab7d219d7a2e191affcd961cd419
Rajan <122583123+rajangithub0@users.noreply.github.com>
2024-11-08 21:57:41 +0530

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import '../Footer/Footer.css'
 const Footer = () => {
     return (
         <div>
-            <div >@styleShere 2024</div>
+            <div >@styleShere 2024made by rajan</div>
         </div>
     )
 }
